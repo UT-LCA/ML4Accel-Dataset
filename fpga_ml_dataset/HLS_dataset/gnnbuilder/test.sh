622f62327bc9460f549afde3e22b063e00af17dc
Stefan Abi-Karam <stefanabikaram@gmail.com>
2023-10-04 12:46:06 -0400

    moved stuff around

--- fpga_ml_dataset/HLS_dataset/gnnbuilder/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-designs=$(find "$SCRIPT_DIR"/designs -maxdepth 1 -type d -print)
-args=""
-for design in $designs; do
-    args+="$SCRIPT_DIR/designs/$design "
-done
-python "$SCRIPT_DIR"/simple_dataset_flow.py --n_jobs 4 "$args" 
