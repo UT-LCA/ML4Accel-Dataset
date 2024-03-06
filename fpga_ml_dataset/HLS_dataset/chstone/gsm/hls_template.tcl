3cfae484827313ba0d5e76f7b99ec44abb45271d
Stefan Abi-Karam <stefanabikaram@gmail.com>
2024-03-06 02:41:23 -0500

    more cleanup and fixes

--- fpga_ml_dataset/HLS_dataset/chstone/gsm/hls_template.tcl
+++ fpga_ml_dataset/HLS_dataset/chstone/gsm/hls_template.tcl
@@ -7,6 +7,6 @@ open_solution -reset solution
 set_part xczu9eg-ffvb1156-2-i
 create_clock -period 10 -name default
 config_rtl -reset state
-# source [opt_tcl]
+source opt.tcl
 csynth_design
 close_project
