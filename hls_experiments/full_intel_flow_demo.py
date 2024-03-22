d6e486860a2c889f263c5fac2634743fcd571e56
nandithaec <nanditha.ec@gmail.com>
2024-03-22 12:14:59 -0500

    C and header file modifications for machsuite

--- hls_experiments/full_intel_flow_demo.py
+++ hls_experiments/full_intel_flow_demo.py
@@ -4,10 +4,13 @@ from pathlib import Path
 from hls_build_framework.flow_vitis import VitisHLSImplFlow, VitisHLSSynthFlow
 from hls_build_framework.framework import DesignDataset
 from hls_build_framework.opt_dsl_frontend_intel import OptDSLFrontendIntel
+from hls_build_framework.intel_machsuite import  AnnotateMachSuiteIntel
+
+
 
 DIR_CURRENT_SCRIPT = Path(__file__).parent
-
-WORK_DIR = Path("/usr/scratch/skaram7/hlsdataset_workdir_intel_test_run")
+parent_dir = "/raid/nanditha/ML4Accel-Dataset/work_space/front_end"
+WORK_DIR = Path("/raid/nanditha/ML4Accel-Dataset/work_space/front_end")
 if WORK_DIR.exists():
     shutil.rmtree(WORK_DIR)
 WORK_DIR.mkdir()
@@ -15,13 +18,13 @@ WORK_DIR.mkdir()
 # DIR_DATASET_POLYBENCH_XILINX = (
 #     DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "polybench"
 # )
-DIR_DATASET_POLYBENCH_INTEL = (
-    DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "polybench"
-)
+#DIR_DATASET_POLYBENCH_INTEL = (
+#    DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "polybench"
+#)
 
-# DIR_DATASET_MACHSUITE_XILINX = Path(
-#     DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "machsuite"
-# )
+DIR_DATASET_MACHSUITE_INTEL = (
+     DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "machsuite"
+)
 
 # DIR_DATASET_CHSTONE_XILINX = Path(
 #     DIR_CURRENT_SCRIPT.parent / "fpga_ml_dataset" / "HLS_dataset" / "chstone"
@@ -41,17 +44,24 @@ DIR_DATASET_POLYBENCH_INTEL = (
 # )
 # dataset_polybench_xilinx = dataset_polybench_xilinx.copy_dataset(WORK_DIR)
 
-dataset_polybench_intel = DesignDataset.from_dir(
-    "polybench_intel",
-    DIR_DATASET_POLYBENCH_INTEL,
-)
-dataset_polybench_intel = dataset_polybench_intel.copy_dataset(WORK_DIR)
+#dataset_polybench_intel = DesignDataset.from_dir(
+#    "polybench_intel",
+#    DIR_DATASET_POLYBENCH_INTEL,
+#)
+#dataset_polybench_intel = dataset_polybench_intel.copy_dataset(WORK_DIR)
 
-# dataset_machsuite_xilinx = DesignDataset.from_dir(
-#     "machsuite_xilinx",
-#     DIR_DATASET_MACHSUITE_XILINX,
-#     exclude_dir_filter=lambda dir: dir.name == "common",
-# )
+
+
+dataset_machsuite_intel = DesignDataset.from_dir(
+     "machsuite_intel",
+     DIR_DATASET_MACHSUITE_INTEL,
+     exclude_dir_filter=lambda dir: dir.name == "common",
+ )
+dataset_machsuite_intel = dataset_machsuite_intel.copy_dataset(WORK_DIR)
+#create intel_src and modify the C and header files for machsuite
+AnnotateMachSuiteIntel.Annotate(WORK_DIR)
+
+
 # dataset_chstone_xilinx = DesignDataset.from_dir(
 #     "chstone_xilinx", DIR_DATASET_CHSTONE_XILINX
 # )
@@ -73,8 +83,8 @@ dataset_polybench_intel = dataset_polybench_intel.copy_dataset(WORK_DIR)
 # }
 
 datasets = {
-    "polybench_intel": dataset_polybench_intel,
-    # "machsuite_xilinx": dataset_machsuite_xilinx,
+ #   "polybench_intel": dataset_polybench_intel,
+     "machsuite_intel": dataset_machsuite_intel,
     # "chstone_xilinx": dataset_chstone_xilinx,
     # "rosetta_xilinx": dataset_rosetta_xilinx,
     # "simple": dataset_simple,
@@ -83,6 +93,8 @@ datasets = {
 opt_dsl_frontend_intel = OptDSLFrontendIntel(
     WORK_DIR, random_sample=True, random_sample_num=10
 )
+
+
 
 designs_after_frontend = {
     dataset_name: opt_dsl_frontend_intel.execute_multiple_designs(
