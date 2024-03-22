2b95baa9fff78168f67282af23f5ec703455d31a
Zhigang Wei <zwei1@kamek.ece.utexas.edu>
2024-03-22 17:59:09 -0500

    integrate intel hls and fixes some small bugs

--- hls_build_framework/flow_intel.py
+++ hls_build_framework/flow_intel.py
@@ -8,7 +8,8 @@ class IntelHLSSynthFlow(ToolFlow):
     def __init__(
         self,
         ipp_bin: str | None = None,
-        arch: str = "Cyclone 10 GX",
+        #arch: str = "Cyclone 10 GX",
+        arch: str = "1ST110EN1F43E1VG",
         clock: str = "10ns",
         verbose: bool = True,
     ):
@@ -16,7 +17,7 @@ class IntelHLSSynthFlow(ToolFlow):
             self.ipp_bin = find_bin_path("i++")
         else:
             self.ipp_bin = ipp_bin
-
+        
         self.arch = arch
         self.clock = clock
         self.verbose = verbose
@@ -24,21 +25,22 @@ class IntelHLSSynthFlow(ToolFlow):
     def execute(self, design: Design) -> list[Design]:
         design_dir = design.dir
         design_name = design.name
+        bench_name = design.dir.name 
 
         # i++ -march="Cyclone 10 GX" -ghdl --clock 10ns --component $benchmark $design.c --simulator "none" -v -o $design
-
+        # i++ -march=1ST110EN1F43E1VG -ghdl "$FILE" --simulator "none" -v -o "$benchmark"_"$i"
         cmd = ""
         cmd += f"{self.ipp_bin} "
         cmd += f"-march={self.arch} "
-        cmd += "-ghdl "
+        cmd += f"-ghdl {design_name} "
         cmd += f"--clock {self.clock} "
-        cmd += f"--component {design_name} "
-        cmd += f"{design_dir / f'{design_name}.c'} "
+        #cmd += f"--component {design_name} "
+        #cmd += f"{design_dir / f'{bench_name}.c'} "
         cmd += '--simulator "none" '
         cmd += "-v "
-        cmd += f"-o {design_name} "
+        cmd += f"-o {design_dir / f'{bench_name}.prj'}"
 
-        call_tool(cmd, design_dir)
+        #call_tool(cmd, design_dir)
 
         return [design]
 
