43ea55f416ecc7426ab9b620d7579fdd045c1850
nandithaec <nanditha.ec@gmail.com>
2024-03-25 12:29:50 -0500

    backprop modified

--- fpga_ml_dataset/HLS_dataset/machsuite/backprop/backprop.c
+++ fpga_ml_dataset/HLS_dataset/machsuite/backprop/backprop.c
@@ -21,9 +21,7 @@ void RELU(TYPE activations[nodes_per_layer], TYPE dactivations[nodes_per_layer],
     }
 }
 
-void add_bias_to_activations(TYPE biases[nodes_per_layer], 
-                               TYPE activations[nodes_per_layer],
-                               int size) {
+void add_bias_to_activations(TYPE biases[nodes_per_layer],  TYPE activations[nodes_per_layer],  int size) {
     int i;
     for( i = 0; i < size; i++){
         activations[i] = activations[i] + biases[i];
