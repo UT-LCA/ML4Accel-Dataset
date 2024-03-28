e37b8ae393d9687c7343f5c113cfa73dbfe4e7da
nandithaec <nanditha.ec@gmail.com>
2024-03-27 20:00:19 -0500

    resources histogram plot

--- hls_experiments/histogram_post_hls.py
+++ hls_experiments/histogram_post_hls.py
@@ -9,30 +9,45 @@ df_hlsyn= pd.read_csv('/home/nanditha/Documents/ML4Accel-Dataset/hls_experiments
   
 
 #df['clk_estimated(ns)'] = df['clk_estimated(ns)'].apply(lambda x: x*100)
-df['clk_estimated(ns)'] = df['clk_estimated(ns)'].apply(lambda x: x*100)
 
+fig, (ax1, ax2,ax3,ax4) = plt.subplots(1, 4)
+fig.suptitle('Horizontally stacked subplots')
 
-# plotting two histograms on the same axis 
-plt.hist(df['ff'], bins=25, alpha=0.5, color='red') 
-plt.hist(df_hlsyn['resources_ff_used'], bins=25, alpha=0.45, color='blue') 
-print ("ff values", df['ff'])  
+ax1.hist(df['ff'], bins=25, alpha=0.8, color='red',label ="our_work")
+ax1.hist(df_hlsyn['resources_ff_used'], bins=40, alpha=0.45, color='blue',label="hlsyn")
+ax1.set_xlabel("Resources- FF used")  
+ax1.set_ylabel("Density")
+ax1.legend(loc='upper right')
+#print ("ff values", df['ff'])  
 
 
 #plotting two histograms on the same axis 
-plt.hist(df['lut'], bins=25, alpha=0.5, color='red')
-plt.hist(df_hlsyn['resources_lut_used'], bins=25, alpha=0.45, color='blue')
-print ("lut values", df['lut'])
+ax2.hist(df['lut'], bins=25, alpha=0.8, color='red',label ="our_work")
+ax2.hist(df_hlsyn['resources_lut_used'], bins=40, alpha=0.45, color='blue',label="hlsyn")
+ax2.set_xlabel("Resources- LUT used")
+ax2.set_ylabel("Density")
+ax2.legend(loc='upper right')
+
+#print ("lut values", df['lut'])
 
 
-plt.hist(df['bram'], bins=25, alpha=0.5, color='red')
-plt.hist(df_hlsyn['resources_bram_used'], bins=25, alpha=0.45, color='blue')
-print ("bram values", df['bram'])
+ax3.hist(df['bram'], bins=25, alpha=0.8, color='red',label ="our_work")
+ax3.hist(df_hlsyn['resources_bram_used'], bins=40, alpha=0.45, color='blue',label="hlsyn")
+ax3.set_xlabel("Resources- LUT used")
+ax3.set_ylabel("Density")
+ax3.legend(loc='upper right')
 
-plt.hist(df['dsp'], bins=25, alpha=0.5, color='red')
-plt.hist(df_hlsyn['resources_dsp_used'], bins=25, alpha=0.45, color='blue')
-print ("dsp values", df['dsp'])
+#print ("bram values", df['bram'])
 
-plt.title("histogram with multiple variables (overlapping histogram)") 
+ax4.hist(df['dsp'], bins=25, alpha=0.8, color='red',label ="our_work")
+ax4.hist(df_hlsyn['resources_dsp_used'], bins=40, alpha=0.45, color='blue',label="hlsyn")
+ax4.set_xlabel("Resources- DSP used")
+ax4.set_ylabel("Density")
+ax4.legend(loc='upper right')
+
+#print ("dsp values", df['dsp'])
+
+#plt.title("histogram with multiple variables (overlapping histogram)") 
   
 #plt.legend(['individuals who are homeless',  'family members who are homeless']) 
   
